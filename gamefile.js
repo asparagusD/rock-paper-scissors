f78e231654443d0d39bb212c7f30f25f1a5a6a18
asparagusD <nufel1130@gmail.com>
2025-01-14 07:46:11 +0600

    Add five more rounds to the game
    Called the playRound function five times to play the game for five rounds and added conditionals to calculate and store results.

--- gamefile.js
+++ gamefile.js
@@ -20,29 +20,45 @@ function playRound(humanChoice,computerChoice) {
     computerChoice = getComputerChoice();
 
     if (humanChoice === "rock" && computerChoice === "scissors") {
-        console.log("You win! Rock beats Scissors!!");
+        console.log("You win the round! Rock beats Scissors!!");
         humanScore++;
     } else if (humanChoice === "rock" && computerChoice === "paper") {
-        console.log("You lose! Paper beats Rock!!");
+        console.log("You lose the round! Paper beats Rock!!");
         computerScore++;
     } else if (humanChoice === "paper" && computerChoice === "scissors") {
-        console.log("You lose! Scissors beat Paper!!");
+        console.log("You lose the round! Scissors beat Paper!!");
         computerScore++;
     } else if (humanChoice === "paper" && computerChoice === "rock") {
-        console.log("You win! Paper beats Rock!!");
+        console.log("You win the round! Paper beats Rock!!");
         humanScore++;
     } else if (humanChoice === "scissors" && computerChoice === "paper") {
-        console.log("You win! Scissors beat Paper!!");
+        console.log("You win the round! Scissors beat Paper!!");
         humanScore++;
     } else if (humanChoice === "scissors" && computerChoice === "rock") {
-        console.log("You lose! Rock beats Scissors!!");
+        console.log("You lose the round! Rock beats Scissors!!");
         computerScore++;
     } else if (humanChoice === computerChoice) {
         console.log("Oops! It's a draw!!");
     }
 }
 
+function playGame() {
+    playRound();
+    playRound();
+    playRound();
+    playRound();
+    playRound();
+    
+    if (humanScore > computerScore) {
+        console.log(`You win the game! Your score is ${humanScore}/5`);
+    } else if (humanScore < computerScore) {
+        console.log(`You lose the game! Your score is ${humanScore}/5`);
+    } else {
+        console.log(`It's a draw by ${humanScore}-${computerScore}!`);
+    }
+}
+
 let humanScore = 0;
 let computerScore = 0;
 
-playRound();
+playGame();
